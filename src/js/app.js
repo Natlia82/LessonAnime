896a23119428aa632b8f4108ea3f6e21318ba334
Natalia <nata@host>
2022-11-06 19:09:43 +0500

    новое

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 // TODO: write your code here
 const ButtonAnime = document.querySelector('.button');
-// const textAnime = document.querySelector('.text');
+
 ButtonAnime.addEventListener('click', () => {
   document.getElementById('expansion-body').classList.toggle('expansion-hide');
-  // document.getElementById('expansion-body').classList.toggle('border');
 });
